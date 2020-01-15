f87f7322d80d1cacaebadefcbe10dce94f0cb83f
Namjun Kim <namjunemy@gmail.com>
2020-01-15 17:55:49 +0900

    Tasklet 구현

---
+++ src/main/java/io/namjune/bootbatch/jobs/InactiveItemTasklet.java
@@ -0,0 +1,42 @@
+package io.namjune.bootbatch.jobs;
+
+import io.namjune.bootbatch.domain.User;
+import io.namjune.bootbatch.domain.enums.UserStatus;
+import io.namjune.bootbatch.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class InactiveItemTasklet implements Tasklet {
+
+    private UserRepository userRepository;
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        // reader
+        Date nowDate = (Date) chunkContext.getStepContext().getJobParameters().get("nowDate");
+        LocalDateTime now = LocalDateTime.ofInstant(nowDate.toInstant(), ZoneId.systemDefault());
+        List<User> inactiveUsers = userRepository.findByUpdatedDateBeforeAndStatusEquals(now.minusYears(1), UserStatus.ACTIVE);
+
+        // processor
+        inactiveUsers = inactiveUsers.stream()
+            .map(User::setInactive)
+            .collect(Collectors.toList());
+
+        // writer
+        userRepository.saveAll(inactiveUsers);
+
+        return RepeatStatus.FINISHED;
+    }
+}
